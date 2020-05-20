03a15bd93e5b3c8a67146f4902fff068479321d9
Aykinn <ayckinn@pm.me>
2020-05-20 09:42:24 +0200

    Add function [del_mac]

--- objects/player.py
+++ objects/player.py
@@ -11,10 +11,12 @@ from design import constants as cst
 class McGyver(object):
 
     def __init__(self, screen):
+        self.bkg = pg.image.load(cst.BKG_PIC).convert_alpha()
         self.macpic = pg.image.load(cst.MACGYVER_PIC).convert_alpha()
         self.macpos = (0, 0)
 
-        screen.master.blit(self.macpic, self.macpos)
+        self.screen = screen
+        self.screen.master.blit(self.macpic, self.macpos)
 
         self.arrows = {pg.K_UP: (-1, 0),
                        pg.K_DOWN: (1, 0),
@@ -24,17 +26,17 @@ class McGyver(object):
     # -------------------------------------------------------------------------
     def del_mac(self):
         y, x = self.macpos
-        screen.master.blit(cst.BKG_PIC, (x * 50, y * 50), (x * 50, y * 50, 50, 50))
+        self.screen.master.blit(self.bkg, (x * 50, y * 50), (x * 50, y * 50, 50, 50))
 
-    # -------------------------------------------------------------------------
-    def update_mac(self, key, lab):
-        y, x = self.macpos
-
-        offy, offx = self.arrows.get(key, (0, 0))
-        if (y + offy, x + offx) in lab:
-            self.macpos = (y + offy, x + offx)
-
-    # -------------------------------------------------------------------------
-    def show_mac(self):
-        y, x = self.macpos
-        screen.master.blit(self.macpic, (x * 50, y * 50))
+    # # -------------------------------------------------------------------------
+    # def update_mac(self, key, lab):
+    #     y, x = self.macpos
+    #
+    #     offy, offx = self.arrows.get(key, (0, 0))
+    #     if (y + offy, x + offx) in lab:
+    #         self.macpos = (y + offy, x + offx)
+    #
+    # # -------------------------------------------------------------------------
+    # def show_mac(self):
+    #     y, x = self.macpos
+    #     self.screen.master.blit(self.macpic, (x * 50, y * 50))
