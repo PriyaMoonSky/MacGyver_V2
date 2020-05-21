ca50ccbab734480914e7f177cf2ef8bd00326fea
Aykinn <ayckinn@pm.me>
2020-05-21 12:11:35 +0200

    Update

--- design/constants.py
+++ design/constants.py
@@ -2,6 +2,8 @@
 # = File containing all the constants of the Game =
 # =================================================
 
+import pygame as pg
+
 # -- GAME WINDOW SETTINGS
 NB_SPRITES = 15
 SPRITE_SIZE = 50
@@ -9,14 +11,14 @@ WINSIZE = NB_SPRITES * SPRITE_SIZE
 GAME_TITLE = "MacGyver Labyrinth - v2.0"
 
 # -- GAME PICS
-BKG_PIC = 'img/background.png'
-FULLWALL_PIC = 'img/fullwall.png'
-WALL_PIC = 'img/wall.png'
-MACGYVER_PIC = 'img/macgyver.png'
-GUARDIAN_PIC = 'img/guardian.png'
-NEEDLE_PIC = 'img/needle.png'
-PIPE_PIC = 'img/pipe.png'
-ETHER_PIC = 'img/ether.png'
-SYRINGE_PIC = 'img/syringe.png'
-YOUWIN_PIC = 'img/youwin.png'
-GAMEOVER_PIC = 'img/gameover.png'
+BKG_PIC = pg.image.load('img/background.png')
+FULLWALL_PIC = pg.image.load('img/fullwall.png')
+WALL_PIC = pg.image.load('img/wall.png')
+MACGYVER_PIC = pg.image.load('img/macgyver.png')
+GUARDIAN_PIC = pg.image.load('img/guardian.png')
+NEEDLE_PIC = pg.image.load('img/needle.png')
+PIPE_PIC = pg.image.load('img/pipe.png')
+ETHER_PIC = pg.image.load('img/ether.png')
+SYRINGE_PIC = pg.image.load('img/syringe.png')
+YOUWIN_PIC = pg.image.load('img/youwin.png')
+GAMEOVER_PIC = pg.image.load('img/gameover.png')
